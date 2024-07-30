00a7c22fe96525fa0e114502b51e7061e2d010c4
tcl <cy.tcl@qq.com>
2024-07-30 21:04:12 +0800

    Thread线程应用，批处理任务

---
+++ src/main/java/com/tcl/demo/service/CouponService.java
@@ -0,0 +1,7 @@
+package com.tcl.demo.service;
+
+
+public interface CouponService
+{
+    public void batchTaskAction();
+}
---
+++ src/main/java/com/tcl/demo/service/CouponServiceV2.java
@@ -0,0 +1,7 @@
+package com.tcl.demo.service;
+
+
+public interface CouponServiceV2
+{
+    public void batchTaskActionV2();
+}
